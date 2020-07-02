23e19faf8cf2257d39a1dae5f897bce1e82f8a11
LeeHyengTae <tedd911@icloud.com>
2020-07-02 10:35:54 +0900

    4단계
    10:35

--- leason/miner.py
+++ leason/miner.py
@@ -9,64 +9,73 @@ import matplotlib.pyplot as plt
 import nltk
 
 @dataclass
-class Entity:
+class Entity: # 왜 entity인가? 서로 다른 언어체계에서 유사한 의미를 갖는 것.
     def __init__(self):
         context: str
         fname: str
         target: str
 
     @property
-    def context(self) -> str: return self.context
+    def context(self) -> str: return self._context
 
     @context.setter
-    def context(self, context): self.context = context
+    def context(self, context): self._context = context
 
     @property
-    def fname(self) -> str: return self.fname
+    def fname(self) -> str: return self._fname
 
     @fname.setter
-    def fname(self, fname): self.fname = fname
+    def fname(self, fname): self._fname = fname
 
     @property
-    def target(self) -> str: return self.target
+    def target(self) -> str: return self._target
 
     @target.setter
-    def target(self, target): self.target = target
+    def target(self, target): self._target = target
 
 
 class Service:
     def __init__(self):
         self.texts = []
         self.tokens = []
+        self.noun_tokens = []
         self.okt = []
         self.stopwords = []
         self.freqtxt = []
         # 행벡터의 의미를 가진다. [리스트]
 
-    def extract_token(self, payload):
+    def extract_texts(self, payload):
         print('>>1 corpus 에서 token 추출')
         filename = payload.context + payload.fname
         with open(filename, 'r', encoding='utf-8') as f:
             self.texts = f.read()
         print(f'1단계 결과물 : {self.texts[:300]}')
 
-    def extract_hanguel(self, payload):
+    def tokenize(self, payload):
         print('>>2 corpus 에서 한글 추출')
         texts = self.texts.replace('\n', ' ')
-        tokenizer = re.compile(r'^[ㄱ-힣]')
+        tokenizer = re.compile(r'[^ㄱ-힣]')
         # r'[]' []하나는 글자 하나를 뜻한다. ^는 not 과 start 두가지 개념이 있음
         # [^]는 not, ^[]은 start 의미로 표현됨.
-        self.texts = tokenizer.sub('', texts)
+        self.texts = tokenizer.sub(' ', texts)
         # 한글이 아닌 ''로만 처리된 값을 tokenizer에 해당하는 것만 담는다.
         print(f'2단계 결과물 : {self.texts[:300]}')
 
     def conversion_token(self):
         print('>>3 한글 token 변환.')
-        pass
+        self.tokens = word_tokenize(self.texts)
+        print(f'3단계 결과물 : {self.tokens[:300]}')
 
     def compound_noun(self):
-        print('>>4 복잡명사화.')
-        pass
+        print('>>4 복합명사화.')
+        arr_ = []
+        for token in self.tokens:
+            token_pos = self.okt.pos(token)
+            _ = [txt_tags[0] for txt_tags in token_pos if txt_tags[1] == 'Noun']
+            if len("".join(_)) > 1:
+                arr_.append("".join(_))
+            self.noun_tokens = " ".join(arr_)
+            print(f'4단계 결과물 : {self.noun_tokens[:300]}')
 
     def extract_stopword(self):
         print('>>5 노이즈 코퍼스에서 토큰 추출')
@@ -97,8 +106,10 @@ class Controller:
     def data_analysis(self):
         entity = Entity()
         service = Service()
-        service.extract_token()
-        service.extract_hanguel()
+        entity.context = './data/'
+        entity.fname = 'kr-Report_2018.txt'
+        service.extract_texts(entity)
+        service.tokenize(entity)
         service.conversion_token() # 한글
         service.compound_noun()
         service.extract_stopword() # 필요없는 단어 추출
@@ -110,6 +121,7 @@ class Controller:
 def print_menu():
     print('0. exit\n')
     print('1. 사전 다운로드\n')
+    print('2. 실행\n')
     return input('메뉴선택\n')
 
 
